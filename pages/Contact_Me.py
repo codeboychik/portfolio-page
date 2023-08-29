1678b63a9889a71cb331a0987f42bbc77dbc2458
Andrey Chernenko <xchernen@mendelu.cz>
2023-08-29 16:41:56 +0300

    simple input validation, adding e-mail subject

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,18 +1,23 @@
+import re
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact me')
 
 with st.form(key='contact_form'):
+    st.info('All fields are mandatory.')
     email = st.text_input('Your email address')
-
-    subject = st.text_input('Enter subject')
-
-    details = st.text_area('Describe some details')
-
+    subject = st.text_input('Enter subject').strip()
+    details = st.text_area('Describe some details').strip()
     submit = st.form_submit_button()
 
     if submit:
-        st.success("Form submitted successfully!")
+        if re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email) is not None\
+                and len(details) > 0 and len(subject) > 0:
+            send_email(receiver=email, subject=subject, details=details)
+            st.success("Form submitted successfully!")
+        else:
+            st.error('An error occured.')
 
 
 
--- send_email.py
+++ send_email.py
@@ -1,22 +1,29 @@
 import smtplib
 import ssl
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
 import dotenv
 
 dotenv.load_dotenv()
 
-host = 'smtp.gmail.com'
-port = 465
 
-username = 'wasd10026@gmail.com'
-password = os.getenv('EMAIL_PASS')
+def send_email(receiver, subject, details):
+    host = 'smtp.gmail.com'
+    port = 465
 
-context = ssl.create_default_context()
+    message = MIMEMultipart()
+    message['From'] = os.getenv('EMAIL_FROM')
+    message['To'] = receiver
+    message['Subject'] = Header(subject, 'utf-8')
+    message.attach(MIMEText(details, 'plain'))
 
-receiver = username
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, 'Hi')
+    password = os.getenv('EMAIL_PASS')
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(message['From'], password)
+        server.sendmail(message['From'], receiver, message.as_string())
 
 
